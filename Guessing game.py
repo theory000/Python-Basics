a1fe3afb1e4ce8333d024780580ede9828b9ae04
salman <theory252@gmail.com>
2024-01-30 09:00:55 +0500

    added a guessing game file

--- Guessing game.py
+++ Guessing game.py
@@ -1,18 +1,17 @@
 import random
-
 secret_number = random.randint(1, 10)
 guesses = 0
 
-while guesses < 5:
-    guess = int(input("Guess a number between 1 and 10: "))
+while guesses <= 5:
+    guess = int(input("Guess a number between 1 to 10: "))
     guesses += 1
 
     if guess == secret_number:
-        print("Congrats! You guessed the number in", guesses, "tries!")
+        print("Congrats you guessed the correct number")
     elif guess < secret_number:
         print("Too low! Try again.")
     else:
         print("Too high! Try again.")
 
 if guess != secret_number:
-    print("Sorry, you ran out of guesses. The number was", secret_number)
+    print("Sorry, you ran out of guesses. The number was ", secret_number)
