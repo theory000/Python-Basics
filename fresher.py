bd8d723ca97f7f46cdd1cbeddeb295edc4d90b74
salman <theory252@gmail.com>
2024-01-30 10:46:51 +0500

    -random num gen with hints

--- fresher.py
+++ fresher.py
@@ -1,41 +1,19 @@
-# Calculator
+import random
 
-# add
-def add(x, y):
-    return x + y
-# subtract
-def sub(x, y):
-    return x - y
-# multiply
-def multi(x, y):
-    return x * y
-# divide
-def div(x, y):
-    if (x == y):
-        return "Cannot divide by zero"
-    else:
-        return x / y
-    
-print("Select operation:")
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Divide")
+random_number = random.randint(1, 100)
+guesses = 0
 
 while True:
-    choice = input("Enter choice (1/2/3/4): ")
+    guess = int(input("Guess a number between 1 to 100: "))
 
-    if choice in ('1', '2', '3' , '4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-
-        if choice == '1':
-            print("Result", add(num1, num2))
-        elif choice == '2':
-            print("Result", sub(num1, num2))
-        elif choice == '3':
-            print("Result", multi(num1, num2))
-        elif choice == '4':
-            print("Result", div(num1, num2))
+    if guess == random_number:
+        print("Congrats! You guessed the number!")
+        break
+    elif guess < random_number:
+        print("Too low! Try again.")
+        if guess >= random_number - 10:
+            print("You're getting close")
     else:
-        print("Invalid input")
+        print("Too high! Try again.")
+        if guess > random_number + 10:
+            print("You're getting close!")
