ccfc084aa621fab15fd1eceaef5f4dd4897d93f7
salman <theory252@gmail.com>
2024-02-17 12:01:55 +0500

    updated

--- fresher.py
+++ fresher.py
@@ -1,16 +1,9 @@
-# Print the list of friends
-friends_list = ["Alice", "Bob", "Carol"]
-print(friends_list)
+scores = []
+for i in range(3):
+    score = int(input("Score: "))
+    scores.append(score)
 
-# Add a new friend to the list
-friends_list.append("Dave")
-print(friends_list)
+print(scores)
 
-# Remove a friend from the list
-friends_list.remove("Bob")
-print(friends_list)
-
-# Check if a friend is in the list
-print("Alice" in friends_list)
-
-print(friends_list)
+average = sum(scores) / len(scores)
+print(f"Average: {average}")
