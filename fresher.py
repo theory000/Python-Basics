fd98d90ff0717f5a799e2fc32c337b2d2d004367
salman <theory252@gmail.com>
2024-02-17 11:51:04 +0500

    updated

--- fresher.py
+++ fresher.py
@@ -1,2 +1,16 @@
-for i in range(3):
-    print("#" * 3)
+# Print the list of friends
+friends_list = ["Alice", "Bob", "Carol"]
+print(friends_list)
+
+# Add a new friend to the list
+friends_list.append("Dave")
+print(friends_list)
+
+# Remove a friend from the list
+friends_list.remove("Bob")
+print(friends_list)
+
+# Check if a friend is in the list
+print("Alice" in friends_list)
+
+print(friends_list)
