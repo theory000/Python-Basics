4999f56832a78adda743e0498f178f4967adb82a
salman <theory252@gmail.com>
2024-02-17 10:35:15 +0500

    updated

--- CS50.py
+++ CS50.py
@@ -62,3 +62,20 @@ y = 13
 z = x / y
 
 print(f"{z:.50f}")
+#--------------------------------
+
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("Not an integer")
+
+
+def main():
+    x = get_int("x: ")
+    y = get_int("y: ")
+
+    print(x + y)
+
+main()
--- fresher.py
+++ fresher.py
@@ -1,15 +1,3 @@
-def get_int(prompt):
-    while True:
-        try:
-            return int(input(prompt))
-        except ValueError:
-            print("Not an integer")
-
-
-def main():
-    x = get_int("x: ")
-    y = get_int("y: ")
-
-    print(x + y)
-
-main()
+for i in range(4):
+    print("?", end="")
+print()
