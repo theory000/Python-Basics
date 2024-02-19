7b4945f103ae096396cd3c3b6210c627bb1e2e00
salman <theory252@gmail.com>
2024-02-19 07:35:16 +0500

    updated

--- CS50.py
+++ CS50.py
@@ -90,5 +90,22 @@ print(scores)
 
 average = sum(scores) / len(scores)
 print(f"Average: {average}")
+#---------------------------------
 
-# 1:44:19
+names = ["Carter", "David", "John"]
+# We'll give a input i.e. "david" and the capatilize() function will make the first word capital and iterate over the array to find that name and in second method use simple if statement.
+name = input("Name: ").capitalize()
+
+# This is First method
+# for n in names:
+#     if name == n:
+#         print("Found")
+#         break
+# else:
+#     print("Not Found")
+
+# This is second method
+if name in names:
+    print("Found")
+else:
+    print("Not Found")
