82eba5282bcef781d7e8013e0126765e0153d3b8
salman <theory252@gmail.com>
2024-02-19 07:59:56 +0500

    updated

--- dict/dictionary.py
+++ dict/dictionary.py
@@ -4,12 +4,28 @@ people = [
     {"name": "John", "number": "0304234486"},
 ]
 
-name = input("Name")
+name = input("Name: ").capitalize()
 
 for person in people:
     if person["name"] == name:
         number = person["number"]
-        print(f"Found {number}")
+        print(f"Found {number}") # OR print(f"Found {person['number']})
         break
+else:
+    print("Not found")
+
+# Simplified Method
+
+peoples = {
+    "Rehan": "030345446577",
+    "Fizze": "030345475448",
+    "Salman": "030348787877",
+}
+
+names = input("Name: ").capitalize()
+
+if names in peoples:
+    numbers = peoples[names]
+    print(f"Found {numbers}")
 else:
     print("Not found")
