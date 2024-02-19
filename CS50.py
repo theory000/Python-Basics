b11e15a1ea662c086d84f1dcc2f48b4db8630cad
salman <theory252@gmail.com>
2024-02-19 08:09:00 +0500

    sys

--- CS50.py
+++ CS50.py
@@ -109,3 +109,36 @@ if name in names:
     print("Found")
 else:
     print("Not Found")
+#---------------------------------
+    
+people = [
+    {"name": "Carter", "number": "03044950486"},
+    {"name": "David", "number": "0354234086"},
+    {"name": "John", "number": "0304234486"},
+]
+
+name = input("Name: ").capitalize()
+
+for person in people:
+    if person["name"] == name:
+        number = person["number"]
+        print(f"Found {number}") # OR print(f"Found {person['number']})
+        break
+else:
+    print("Not found")
+
+# Simplified Method
+
+peoples = {
+    "Rehan": "030345446577",
+    "Fizze": "030345475448",
+    "Salman": "030348787877",
+}
+
+names = input("Name: ").capitalize()
+
+if names in peoples:
+    numbers = peoples[names]
+    print(f"Found {numbers}")
+else:
+    print("Not found")
--- fresher.py
+++ fresher.py
@@ -1,17 +1,6 @@
-names = ["Carter", "David", "John"]
-# We'll give a input i.e. "david" and the capatilize() function will make the first word capital and iterate over the dir to find that name and in second method use simple if statement.
-name = input("Name: ").capitalize()
+from sys import argv
 
-# This is First method
-# for n in names:
-#     if name == n:
-#         print("Found")
-#         break
-# else:
-#     print("Not Found")
-
-# This is second method
-if name in names:
-    print("Found")
+if len(argv) == 2:
+    print(f"hello, {argv[1]}")
 else:
-    print("Not Found")
+    print("hello, world")
