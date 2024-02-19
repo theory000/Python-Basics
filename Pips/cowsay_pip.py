bd71fcf0556f39221790420a4a563e733454eb52
salman <theory252@gmail.com>
2024-02-19 08:52:44 +0500

    QR code generator with Python

--- Pips/cowsay_pip.py
+++ Pips/cowsay_pip.py
@@ -2,4 +2,9 @@ import cowsay
 
 name = input("What's your name? ")
 cowsay.cow(f"hello, {name}")
-cowsay.dragon(f"hello, {name}")
+
+# Generating QR code with python
+import qrcode
+
+img = qrcode.make("https://youtu.be/EHi0RDZ31VA?si=WjU8-CPJmW3vMB1y")
+img.save("qr.png", "PNG") # Now a QR should be generated in the parent folder of this file
