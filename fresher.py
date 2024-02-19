0cf6bb8f8a43b2300777eb8d2080c80d5240b8b9
salman <theory252@gmail.com>
2024-02-19 07:33:46 +0500

    updated

--- fresher.py
+++ fresher.py
@@ -1,9 +1,17 @@
-scores = []
-for i in range(3):
-    score = int(input("Score: "))
-    scores.append(score) # OR scores = scores + [score]
+names = ["Carter", "David", "John"]
+# We'll give a input i.e. "david" and the capatilize() function will make the first word capital and iterate over the dir to find that name and in second method use simple if statement.
+name = input("Name: ").capitalize()
 
-print(scores)
+# This is First method
+# for n in names:
+#     if name == n:
+#         print("Found")
+#         break
+# else:
+#     print("Not Found")
 
-average = sum(scores) / len(scores)
-print(f"Average: {average}")
+# This is second method
+if name in names:
+    print("Found")
+else:
+    print("Not Found")
